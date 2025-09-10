ca55e09f9aa1ebf515911b99d38c48d6b0decf26
Oliver-WD <oliverdavies2000@gmail.com>
2025-09-10 14:50:44 +0200

    ipynb deze noten in je gezicht

--- Exercise1.py
+++ Exercise1.py
@@ -1,15 +1,32 @@
 import nltk
-from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+nltk.download("wordnet")
+nltk.download("omw-1.4")
+import spacy
 
-ps = PorterStemmer()
+wnl = WordNetLemmatizer()
 
 word_types = []
 
 with open("sorted_types_HW1.txt") as f:
     for word in f:
-        lemma = ps.stem(word.strip())
+        lemma = wnl.lemmatize(word.strip())
         if lemma not in word_types:
             word_types.append(lemma)
+
+print(len(word_types))
+print(word_types)
+
+word_types = []
+nlp = spacy.load("en_core_web_sm")
+lemmatizer = nlp.get_pipe("lemmatizer")
+with open("sorted_types_HW1.txt") as f:
+    doc = nlp(f.read())
+    for token in doc:
+        if token.lemma_ not in word_types:
+            word_types.append(token.lemma_)
+        else:
+            print(str(token) + " -> " + str(token.lemma_))
 
 print(len(word_types))
 print(word_types)
